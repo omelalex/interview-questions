be3f8d71bd12600890c93e4d644e239238bbe57c
Alex Omelchenko <v-olomel@microsoft.com>
2019-03-12 10:47:08 -0700

    Added Question3

---
+++ ConsoleApp1/Question3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Question3
+    {
+        public class Job
+        {
+            public Guid Id { get; set; }
+
+            public DateTime StartTime { get; set; }
+
+            public DateTime EndTime { get; set; }
+        }
+
+        public int MaxJobsRunningSimultaneously(IEnumerable<Job> jobs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
